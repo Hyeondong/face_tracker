5905aac005b4c0be56ca046c879a7f4e69c2a2ea
Hyeondong <slehd23@gmail.com>
2018-11-20 23:38:05 +0900

    _

--- drone_pi/pi-face-recognition/encode_faces.py
+++ drone_pi/pi-face-recognition/encode_faces.py
@@ -21,23 +21,16 @@ knownEncodings = []
 knownNames = []
 
 for (i, imagePath) in enumerate(imagePaths):
-	print("[INFO] processing image {}/{}".format(i + 1,
-		len(imagePaths)))
+	print("[INFO] processing image {}/{}".format(i + 1, len(imagePaths)))
 	name = imagePath.split(os.path.sep)[-2]
-
 	image = cv2.imread(imagePath)
 	rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
 	boxes = face_recognition.face_locations(rgb,
 		model=args["detection_method"])
-
 	encodings = face_recognition.face_encodings(rgb, boxes)
-
 	for encoding in encodings:
 		knownEncodings.append(encoding)
 		knownNames.append(name)
-
-print("[INFO] serializing encodings...")
 data = {"encodings": knownEncodings, "names": knownNames}
 f = open(args["encodings"], "wb")
 f.write(pickle.dumps(data))
